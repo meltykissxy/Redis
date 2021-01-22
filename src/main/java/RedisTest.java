ec8793a308cbac94b7909b7fa60a96ca682bd82a
MeltyKiss <xymeltykiss@gmail.com>
2021-01-22 22:22:12 +0800

    其他数据类型的操作

--- src/main/java/RedisTest.java
+++ src/main/java/RedisTest.java
@@ -10,16 +10,95 @@ import java.util.Set;
 public class RedisTest {
 
     @Test
+    public void run09() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+
+        jedis.close();
+    }
+
+    @Test
+    public void run08() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+
+        jedis.close();
+    }
+
+    @Test
+    public void run07() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+        jedis.zadd("z1", 5, "lss");
+        jedis.zadd("z1", 9, "yjy");
+        jedis.zadd("z1", 3, "dxy");
+        jedis.zadd("z1", 8, "ly");
+
+        System.out.println(jedis.zcard("z1"));
+
+        jedis.zadd("z1", 9, "yjy");
+        System.out.println(jedis.zcard("z1"));
+
+        jedis.zadd("z1", 10, "yjy");
+        System.out.println(jedis.zcard("z1"));
+
+        jedis.close();
+    }
+
+    @Test
+    public void run06() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+        jedis.hset("h1", "name", "lss");
+        jedis.hset("h1", "age", "18");
+        jedis.hset("h1", "gender", "girl");
+
+        System.out.println(jedis.hgetAll("h1"));
+
+        jedis.close();
+    }
+
+    @Test
+    public void run05() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+        jedis.sadd("s1", "lss", "yjy", "lxy", "dxy");
+        System.out.println(jedis.smembers("s1"));
+        jedis.close();
+    }
+
+    @Test
+    public void run04() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+        jedis.flushDB();
+        jedis.set("k1", "v1");
+        jedis.get("k1");
+        // KISS 重命名key
+        jedis.rename("k1", "k2");
+        System.out.println(jedis.get("k2"));
+        System.out.println(jedis.get("k1"));
+        jedis.close();
+    }
+
+    @Test
+    public void run03() {
+        Jedis jedis = new Jedis("hadoop102",6379);
+        // KISS 你看，其实你可以根据返回值1或0来判断之前有没有这个key，去重时就可以利用这个技巧哦
+        System.out.println(jedis.setnx("s1", "18")); // 1
+        System.out.println(jedis.setnx("s1", "16")); // 0
+        jedis.close();
+    }
+
+    @Test
     public void run02() {
         Jedis jedis = new Jedis("hadoop102",6379);
         // KISS 简单的赋值，注意这里可以返回成功还是失败
         System.out.println(jedis.set("k1", "lss"));
         System.out.println(jedis.set("k2", "ly"));
         System.out.println(jedis.set("k3", "yjy"));
+        System.out.println(jedis.set("a1", "dh"));
+        System.out.println(jedis.set("b2", "chj"));
         // KISS 简单的获取key对应的值
         System.out.println(jedis.get("k1"));
         // KISS 获取所有key
         System.out.println(jedis.keys("*").toString());
+        System.out.println(jedis.keys("k*").toString());
+        System.out.println(jedis.keys("*1").toString());
         jedis.close();
     }
 
